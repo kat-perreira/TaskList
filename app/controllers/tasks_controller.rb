aa31bccd7cac50a204080d7346a558a70c4e3afa
Kat Perreira <kat.perreira@gmail.com>
2018-09-17 16:24:12 -0700

    added show instance variable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,19 @@
 
 
-TASKS =["task_one", "task_two", "task_three"]
+TASKS =["meow", "hiss", "purr"]
 
 class TasksController < ApplicationController
 
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task= TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
   end
 
 end
