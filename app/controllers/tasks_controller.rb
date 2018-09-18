748a6c351a203e1838319f8a39967b9cc646291f
Kat Perreira <kat.perreira@gmail.com>
2018-09-18 15:25:28 -0700

    changed view to reflect new additions in model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,16 @@
 
 
-TASKS =["meow", "hiss", "purr"]
+
 
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
-    task_id = params[:id].to_i
-    @task= TASKS[task_id]
+    task_id = params[:id]
+    @task= Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
     end
