78ed99e8dcaa3b0eb0cb4e72fb8e384a7525f369
Kat Perreira <kat.perreira@gmail.com>
2018-09-20 18:16:52 -0700

    added in new, update feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,39 @@ class TasksController < ApplicationController
     end
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(
+      name: params[:task][:name],
+      completion_date: params[:task][:completion_date],
+      description: params[:task][:description]
+    ) #instantiate a new task
+
+    save_success = task.save
+
+    if save_success # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the book in the list
+    else # save failed
+      render :new # show the new book form view again
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+      @task.update(
+        name: params[:task][:name],
+        completion_date: params[:task][:completion_date],
+        description: params[:task][:description]
+      )
+
+        redirect_to task_path(@task)
+  end
+
 end
