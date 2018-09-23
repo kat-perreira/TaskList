5f4141c52df1900d2af6fc231a2cc7f632a6e74d
Kat Perreira <kat.perreira@gmail.com>
2018-09-23 10:50:16 -0700

    added column to table for date completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_184934) do
+ActiveRecord::Schema.define(version: 2018_09_23_172933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_22_184934) do
     t.datetime "updated_at", null: false
     t.date "completion_date"
     t.boolean "completed"
+    t.date "date_completed"
   end
 
 end
